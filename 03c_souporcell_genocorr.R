c07f779158b88c900b737ef01812e2c6fb6390ea
Daniel Silva Araujo <daraujo@midway3-login3.rcc.local>
2024-02-09 14:15:14 -0600

    update parameters

--- 03c_souporcell_genocorr.R
+++ 03c_souporcell_genocorr.R
@@ -14,22 +14,22 @@ for (i in 1:length(input_f)){
 
   #job file header
   sbatch_topper <- '#!/bin/sh' %&% '\n' %&% '#SBATCH --time=36:00:00' %&% '\n' %&%
-    '#SBATCH --mem=100G' %&% '\n' %&% '#SBATCH --partition=caslake' %&% '\n' %&%
-    '#SBATCH --account=pi-lbarreiro' %&% '\n' %&% '#SBATCH --nodes=10' %&% '\n' %&% 
+    '#SBATCH --mem=120G' %&% '\n' %&% '#SBATCH --partition=caslake' %&% '\n' %&%
+    '#SBATCH --account=pi-lbarreiro' %&% '\n' %&% '#SBATCH --nodes=1' %&% '\n' %&%
     '#SBATCH --error='%&% souporcell_folder %&%'/souporcell_genocorr_' %&% input_f[i] %&% '.error' %&% '\n' %&%
     '#SBATCH --out='%&% souporcell_folder %&%'/souporcell_genocorr_' %&% input_f[i] %&% '.out' %&%
     '\n\n' %&% 'module load singularity'
-  
+
   #command line
   command_line <- 'singularity exec --bind ' %&% project_folder %&% ' ' %&%
     souporcell_folder %&% 'Demuxafy.sif Assign_Indiv_by_Geno.R -r ' %&%
-    vcf_file %&% ' -c ' %&% souporcell_outdir %&% '/cluster_genotypes.vcf ' %&% 
+    vcf_file %&% ' -c ' %&% souporcell_outdir %&% '/cluster_genotypes.vcf ' %&%
     '-o ' %&% souporcell_outdir
-  
+
   #create sbatch file and write into it
   job_file <- file.path(souporcell_folder, input_f[i] %&% '_souporcell_genocorr_job.sbatch')
   cat(sbatch_topper %&% '\n\n' %&% command_line, file=job_file, append=F)
-  
+
   #launch job!
   launch_call <- 'sbatch ' %&% job_file
   system(launch_call) #launches the job into midway
